03d1eb73340b36906f28baf8f5413d0ee1af9d79
WafaringStranger <shubhampaitrik8@gmail.com>
2020-06-16 13:03:45 +0530

    Prime Factors

---
+++ Python/Examples/celcius_to_farenheit.py
@@ -0,0 +1,5 @@
+# Python Program to Take the Temperature in Celcius and Covert it to Ferenheit.
+
+celcius = float(input("Enter the temprature in celcius: "))
+farenheit = (celcius * 1.8) + 32
+print("Temprature in farenheit is ", round(farenheit, 2))
