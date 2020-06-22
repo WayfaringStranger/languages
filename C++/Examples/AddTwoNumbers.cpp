b96c8d46b09780e2c64ca9f8394b7f3d525dce76
WafaringStranger <shubhampaitrik8@gmail.com>
2020-06-22 15:13:41 +0530

    Add Two Numbers CPlusPlus

---
+++ C++/Examples/AddTwoNumbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int firstNumber, secondNumber, sumOfNumbers = 0;
+	cout << "Enter Numbers: ";
+	cin >> firstNumber >> secondNumber;
+	sumOfNumbers = firstNumber + secondNumber;
+	cout << "Sum of Numbers is " << sumOfNumbers << endl;
+	return 0;
+}
