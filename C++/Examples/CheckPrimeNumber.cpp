c7011e4a7e283b2a17e230b283ba7405f446a174
WafaringStranger <shubhampaitrik8@gmail.com>
2020-06-29 17:23:33 +0530

    Check Prime Number

---
+++ C++/Examples/CheckPrimeNumber.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, i;
+	bool isPrime = true;
+	cout << "Enter a positive integer: ";
+	cin >> n;
+	for (i = 2; i <= n/2; i++) {
+		if (n%i == 0) {
+			isPrime = false;
+			break;
+		}
+	}
+	if (isPrime)
+		cout << n << " is a prime number." << endl;
+	else
+		cout << n << " isn't prime number." << endl;
+	return 0;
+}
